59d43010f524bc098109c09007a975589a6aaeb2
pablowinck <pablowinck123@gmail.com>
2023-09-20 11:14:50 -0300

    feat: wait-for-file.sh has max tries

--- src/main/resources/compilador-metricas/wait-for-file.sh
+++ src/main/resources/compilador-metricas/wait-for-file.sh
@@ -12,10 +12,25 @@ FILE_NAME="$1"
 # Nome do arquivo sem o caminho do diretório
 BASE_NAME=$(basename "$FILE_NAME")
 
+# Máximo de tentativas
+MAX_TRIES=20
+
+# Contador de tentativas
+TRIES=0
+
 # Loop para verificar se o arquivo existe & se tem conteudo no arquivo
 while [ ! -s "$FILE_NAME" ]; do
-    echo "Arquivo $BASE_NAME ainda não foi criado. Aguardando..."
-    sleep 10
+    # Incrementa o contador de tentativas
+    TRIES=$((TRIES + 1))
+
+    # Verifica se o número máximo de tentativas foi atingido
+    if [ $TRIES -ge $MAX_TRIES ]; then
+        echo "Número máximo de tentativas alcançado. Saindo..."
+        exit 1
+    fi
+
+    echo "Tentativa $TRIES: Arquivo $BASE_NAME ainda não foi criado. Aguardando..."
+    sleep 60
 done
 
 # Mensagem quando o arquivo é criado
