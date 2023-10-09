c55319cad3c4bf9b93ccd3e9dee8a6556e92a7ea
pablowinck <pablowinck123@gmail.com>
2023-10-09 17:11:14 -0300

    feat: data limite submissao

--- src/main/java/com/muralis/rinhacontrolesubmissoes/core/usecase/AtualizarSubmissaoRanking.java
+++ src/main/java/com/muralis/rinhacontrolesubmissoes/core/usecase/AtualizarSubmissaoRanking.java
@@ -8,6 +8,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 @Log4j2
 @RequiredArgsConstructor
@@ -15,11 +17,15 @@ public class AtualizarSubmissaoRanking {
 
 	private final RankingRepository rankingRepository;
 
+	private static final LocalDate dataLimite = LocalDate.of(2023, 10, 8);
+
 	@EventListener
 	public void on(SubmissaoProcessada submissaoProcessada) {
 		Submissao submissao = submissaoProcessada.getSubmissao();
 		log.info("Atualizando ranking com submissao {}", submissao);
 		rankingRepository.findByUserId(submissao.getUserId()).ifPresentOrElse(ranking -> {
+			if (submissao.getDataEnvio().toLocalDate().isAfter(dataLimite))
+				return;
 			if (ranking.notaMaiorQue(submissao.getNota())) {
 				log.info("Submissao {} não altera ranking", submissao);
 				return;
